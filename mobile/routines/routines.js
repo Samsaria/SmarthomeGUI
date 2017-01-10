88954a4811f5f5b15780c72ac122a116c9274c04
Thomas Trügler <thomas.truegler@symvaro.com>
2017-01-10 17:49:31 +0100

    merge and routines clickable and time

--- mobile/routines/routines.js
+++ mobile/routines/routines.js
@@ -2,3 +2,14 @@
 $(document).ready(() =>
     createListFromFile("#routine-list", ".template > li", "routines.json")
 );
+
+var templateElement = $(templateSelector);
+
+templateElement.onclick({
+
+});
+
+function routineExecuted(){
+    swal("Successfully executed!!", "Wait for your devices to perform!", "success");
+}
+>>>>>>> Stashed changes
