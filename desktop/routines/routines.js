24a160cf8e2ca21162f11250770163cc1b679137
Thomas Truegler <thomas.truegler@gmx.net>
2017-01-24 15:59:15 +0100

    felf

--- desktop/routines/routines.js
+++ desktop/routines/routines.js
@@ -3,12 +3,8 @@ $(document).ready(function() {
     createListFromFile("#routine-list", ".template > li", "../../data/routines.json");
 });
 
-var templateElement = $(templateSelector);
-
-templateElement.onclick({
-
-});
-
 function routineExecuted() {
     swal("Successfully executed!!", "Wait for your devices to perform!", "success");
 }
+
+
