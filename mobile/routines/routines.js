188389c5d020b222941607294da85856b7798d4b
Fabian <schoberf@gmx.net>
2017-01-25 22:44:21 +0100

    Routine execution

--- mobile/routines/routines.js
+++ mobile/routines/routines.js
@@ -1,19 +1,41 @@
 const dataFilePath = '../../data/routines.json';
 
 //Load data
-$(document).ready(function() {
+$(document).ready(function () {
     var localStorageRoutines = JSON.parse(localStorage.getItem('routines'));
     if (localStorageRoutines) {
         createListFromData("#routine-list", ".template > li", localStorageRoutines, "routines");
     } else {
         createListFromFile("#routine-list", ".template > li", dataFilePath, "routines")
-            .then(() =>  sortByName("#device-list", true));
+            .then(() => sortByName("#device-list", true));
     }
 
 });
 
-function routineExecuted() {
+function routineExecuted(element, id) {
+    let routines = JSON.parse(localStorage.getItem('routines'));
+    var routine = getObjects(routines, 'id', id)[0];
+
+    let storageDevices = JSON.parse(localStorage.getItem('devices'));
+    for (let routineDevice of routine.devices) {
+        let storageDevice = getObjects(storageDevices, "id", routineDevice.id);
+        if (storageDevice.length)
+            storageDevice[0].state = routineDevice.new_state;
+    }
+
+    writeTolocalStorage("devices", storageDevices);
     swal("Successfully executed", "Wait for your devices to perform.", "success");
 }
 
-
+function getObjects(obj, key, val) {
+    var objects = [];
+    for (var i in obj) {
+        if (!obj.hasOwnProperty(i)) continue;
+        if (typeof obj[i] == 'object') {
+            objects = objects.concat(getObjects(obj[i], key, val));
+        } else if (i == key && obj[key] == val) {
+            objects.push(obj);
+        }
+    }
+    return objects;
+}
